6f003b8452b0345c11cb7d9506562758bacea087
Ahmed Kamran <ahmedmuyen.kamran@aecicharterhs.org>
2017-10-01 19:20:55 +0000

    changed more

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,28 +3,29 @@ class TodoController < ApplicationController
         
     end
     def show
-        if params[:id]=="1"
-            @task="Do Chemistry homework"
-        end
+            if params[:id]=="1"
+                @task="Do Chemistry homework"
+            end
             if params[:id]=="2"
                 @task="Do Algebra homework"
             end
-                if params[:id]=="3"
-                    @task="Finish NHS survey"
-                end    
-                    if params[:id]=="4"
-                        @task="Finish scholarship application"
-                    end
+            if params[:id]=="3"
+                @task="Finish NHS survey"
+            end    
+            if params[:id]=="4"
+                @task="Finish scholarship application"
+            end
+            @order=params[:order]
     end
     
         def new
         end
-            def create
-                t = Todo.new
-                t.description = params['description']
-                t.order = params['order']
-                t.save
-                redirect_to "/todo/show/#{ t.id }"
+        def create
+            t = Todo.new
+            t.description = params['description']
+            t.order = params['order']
+            t.save
+            redirect_to "/todo/show/#{ t.id }"
 
-            end
+        end
 end
